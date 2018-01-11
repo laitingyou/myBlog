402a5208875c170ebd85ddce3faa589853220031
laitingyou <lty5201314>
2018-01-11 15:04:54 +0800

    #

--- myblog/src/index.js
+++
@@ -1,8 +0,0 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import './index.css';
-import Router from './router/index';
-import registerServiceWorker from './registerServiceWorker';
-import 'antd/dist/antd.css';
-ReactDOM.render(<Router />, document.getElementById('root'));
-registerServiceWorker();
